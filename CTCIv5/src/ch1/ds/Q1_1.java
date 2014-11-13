7570fb0586e9e0becdd33aa060b77282589965cb
saeedzareian <zareian@yorku.ca>
2014-11-12 23:24:44 -0500

    Done 12/11/2014

--- CTCIv5/src/ch1/ds/Q1_1.java
+++ CTCIv5/src/ch1/ds/Q1_1.java
@@ -1,10 +1,47 @@
 package ch1.ds;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+/***
+ * 
+ * @author saeed
+ *	returns no when there is duplicates, yes in duplicates
+ */
 public class Q1_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");
-	}
+		
+ 	 // part1();
+ 	  part2();
 
+	}
+	public static void part1(){
+	       Scanner s= new Scanner(System.in);	
+	       String str=  s.nextLine();
+	       ArrayList<Character> list = new ArrayList<Character>();
+	       for(int i=0; i<str.toCharArray().length; i++){
+	    	   if(list.contains(str.charAt(i))){
+	    		  System.out.println("No");
+	    		  return;
+	    	   }
+	    	   list.add(str.charAt(i));
+	       }
+	 	  System.out.println("Yes");
+	}
+	public static void part2(){
+	       Scanner s= new Scanner(System.in);	
+	       String str=  s.nextLine();
+	       char[] string = str.toCharArray();
+	       Arrays.sort(string);
+	       char last = string[0];
+	       for(int i=1; i<string.length;i++){
+	    	   if(last==string[i]){
+	    		   System.out.println("NO");
+	    		   return ; 
+	    	   }
+	    	   last = string[i];
+	       }
+	 	  System.out.println("Yes");
+	}
 }
